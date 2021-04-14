66efc65fee73cf546ece860ca33016c0d999e0b0
Uli Horn <ulihorn@gmail.com>
2021-04-14 11:33:33 +0200

    reformated hello.py using black

--- hello.py
+++ hello.py
@@ -3,24 +3,23 @@
 """This is a test"""
 
 import click
-#import glob
+
+# import glob
 
 from mylib.myadd import add
 
+
 @click.command()
-@click.option('--value1', help='The first number to add')
-@click.option('--value2', help='The second number to add')
+@click.option("--value1", help="The first number to add")
+@click.option("--value2", help="The second number to add")
 def calculate(value1, value2):
     """This adds two numbers: --value1 and --value2"""
     result = add(value1, value2)
 
-    click.echo(
-        click.style(f"Adding numbers: {value1}, {value2}", bg="black", fg="red")
-    )
-    click.echo(
-        click.style(f"Result: {result}", fg="blue")
-    )
+    click.echo(click.style(f"Adding numbers: {value1}, {value2}", bg="black", fg="red"))
+    click.echo(click.style(f"Result: {result}", fg="blue"))
+
 
 if __name__ == "__main__":
-    #pylint: disable=no-value-for-parameter
+    # pylint: disable=no-value-for-parameter
     calculate()
