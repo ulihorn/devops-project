5becc51594b7b3f4e6a306b6c08f6865ad06ea09
Uli Horn <ulihorn@gmail.com>
2021-04-14 14:56:55 +0200

    adding cli tests

--- Makefile
+++ Makefile
@@ -6,12 +6,10 @@ install:
 		pip install -r requirements.txt
 
 test:
-	#python -m pytest test_gcli.py
-	python -m pytest -vv --cov=helloclick test_helloclick.py
-	#python -m pytest --nbval notebook.ipynb
+	python -m pytest -vv --cov=mylib --cov=hello test_hello.py
 
 lint:
-	pylint --disable=R,C,E1120 hello.py
+	pylint --disable=R,C,E1120 hello.py mylib/*.py
 	
 format:
 	black *.py
--- hello.py
+++ hello.py
@@ -10,15 +10,16 @@ from mylib.myadd import add
 
 
 @click.command()
-@click.option("--value1", help="The first number to add")
-@click.option("--value2", help="The second number to add")
-def calculate(value1, value2):
+@click.option("--value1", default=1, help="The first number to add")
+@click.option("--value2", default=1, help="The second number to add")
+@click.argument("name")
+def calculate(value1, value2, name):
     """This adds two numbers: --value1 and --value2"""
     result = add(value1, value2)
 
     click.echo(click.style(f"Adding numbers: {value1}, {value2}", bg="black", fg="red"))
     click.echo(click.style(f"Result: {result}", fg="blue"))
-
+    click.echo(click.style(f"Name: {name}", bg="black", fg="green"))
 
 if __name__ == "__main__":
     # pylint: disable=no-value-for-parameter
